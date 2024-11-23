b28b555a41f20f8f7cb5758666d955583b1d9bc4
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2024-11-23 12:00:44 +0900

    feat:플레이리스트 url 추가

--- movie_backend/accounts/urls.py
+++ movie_backend/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from movies.views import PlaylistView
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
@@ -11,4 +12,6 @@ urlpatterns = [
     path('profile/<int:user_id>/rcomments/', views.UserReviewCommentView.as_view(), name='user_rcomments'),
     path('profile/<int:user_id>/movies/', views.UserMovieView.as_view(), name='user_moviess'),
     path('users/<int:user_id>/follow', views.UserFollowView.as_view(), name='follow'),
+    path('profile/<int:user_id>/playlists/', PlaylistView.as_view(), name='user_playlists'),
+    path('profile/<int:user_id>/playlists/<int:playlist_id>/', PlaylistView.as_view(), name='playlist_detail'),
 ]
