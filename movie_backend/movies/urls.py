db7197e2cc6e8e8aae12681fbab8f4af916ab3da
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2024-11-24 03:04:33 +0900

    fix: urls에 reviews/likes제거

--- movie_backend/movies/urls.py
+++ movie_backend/movies/urls.py
@@ -5,9 +5,8 @@ urlpatterns = [
     path('movie_list/', views.MovieListView.as_view(), name='movie_list'),
     path('movie_detail/<int:movie_id>/', views.MovieDetailView.as_view(), name='movie_detail'),
     path('movie_search/', views.MovieSearchView.as_view(), name='movie_search'),
-    path('emotion/<str:emotion>', views.SelectedEmotionView.as_view(), name='emotion'),\
+    path('emotion/<str:emotion>', views.SelectedEmotionView.as_view(), name='emotion'),
     path('reviews/', views.ReviewView.as_view(), name='reviews'),
-    path('reviews/likes/', views.ReviewView.as_view(), name='review_like'),
     path('reviews/<int:review_id>/', views.ReviewView.as_view()),
     path('reviews/<int:review_id>/comments/', views.ReviewCommentView.as_view(), name='review_comment'),
     path('comments/<int:comment_id>/', views.ReviewCommentView.as_view(), name='comment_detail'),
